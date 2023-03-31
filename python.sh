0ae08b72a4a678ea7ef7fe690215ff62861be7c0
ChloeLidec <chloe.lidec@etu.univ-orleans.fr>
2023-03-31 09:04:32 -0400

    closes #2 - L'utilisateur peut faire les installation python comme il désire et voit un menu clair

--- python.sh
+++ python.sh
@@ -1,48 +1,45 @@
 #!/bin/bash
 
 # Reset
-Color_Off='\033[0m'       # Text Reset
+COLOR_OFF='\033[0m'       # Text Reset
 
 # Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install -y python3-pip
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
 
-selection = $(whiptail --title "Python" --checklist \
-"Choose what you want to install" 15 60 4 \
-"Basic python vscode extension" "" ON \
+# sudo apt-get update && sudo apt-get upgrade -y -> cette ligne sera faite dans le programme principal
+
+eval SELECTION=( $(whiptail --title "Python" \
+--checklist "Choose what you want to install" 25 80 8 \
+"Python" "" ON \
+"Basic python vscode extension" "" OFF \
 "Autodocstring vscode extension" "" OFF \
 "Pylance vscode extension" "" OFF \
 "Pytest" "" OFF \
 "Pylint" "" OFF \
-"Pygame" "" OFF 3>&1 1>&2 2>&3)
+"Pygame" "" OFF 3>&1 1>&2 2>&3))
 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    array=($selection)
-    for i in "${array[@]}"
-        if $i == "Basic python vscode extension"; then
+if [ "${#SELECTION[@]}" != 0 ]; then
+    for choice in "${SELECTION[@]}"; do
+        if [ $choice == "Python" ]; then
+            sudo apt install -y python3
+            sudo apt install -y python3-pip
+        elif [ $choice == "Basic python vscode extension" ]; then
             code --install-extension ms-python.python
-        elif $i == "Autodocstring vscode extension"; then
+        elif [ $choice == "Autodocstring vscode extension" ]; then
             code --install-extension njpwerner.autodocstring
-        elif $i == "Pylance vscode extension"; then
+        elif [ $choice == "Pylance vscode extension" ]; then
             code --install-extension ms-python.vscode-pylance
-        elif $i == "Pytest"; then
+        elif [ $choice == "Pytest" ]; then
             sudo apt install -y python3-pytest
-        elif $i == "Pylint"; then
+        elif [ $choice == "Pylint" ]; then
             sudo apt install -y pylint
-        elif $i == "Pygame"; then
+        elif [ $choice == "Pygame" ]; then
             pip install pygame
         fi
+    done
+    echo -e "${GREEN} Installation complete ${COLOR_OFF}"
 else
-    echo "${Red} User selected Cancel. ${Color_Off}"
+    echo -e " ${RED} User selected Cancel. ${COLOR_OFF}"
 fi
 
-echo "${Green} Installation complete ${Color_Off}"
