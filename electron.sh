ac526830310181a3624f34d581743e07ecfe0e55
ChloeLidec <chloe.lidec@etu.univ-orleans.fr>
2023-04-03 14:42:31 -0400

    #8 - Le code fonctionne, le probleme vient de la version de ma vm + ajout menu selection avant install

--- electron.sh
+++ electron.sh
@@ -9,34 +9,45 @@ GREEN='\033[0;32m'        # Green
 YELLOW='\033[0;33m'       # Yellow
 BLUE='\033[0;34m'         # Blue
 
-eval SELECTION=( $(whiptail --title "Electron" --checklist \
-"Choose what you want to install" 25 80 8 \
-"Node.js" "" ON \
-"NPM" "" ON \
-"Electron" "" ON \
-"Npm intellisense" "" OFF \
-"ESLint" "" OFF 3>&1 1>&2 2>&3))
+CHOIX=$(whiptail --title "Electron" --menu "Choose what you want to do" 25 80 8 \
+"See electron installations" "" \
+"Init electron rep" "" 3>&1 1>&2 2>&3)
 
-if [ "${#SELECTION[@]}" != 0 ]; then
-    for choice in "${SELECTION[@]}"; do
-        if [ "$choice" == "Node.js" ]; then
-            sudo apt-get install -y nodejs
-            echo -e "${GREEN} Node.js installation done ${COLOR_OFF}"
-        elif [ "$choice" == "NPM" ]; then
-            sudo apt install -y npm
-            echo -e "${GREEN} NPM installation done ${COLOR_OFF}"
-        elif [ "$choice" == "Electron" ]; then
-            sudo npm install electron --save-dev
-            echo -e "${GREEN} Electron installation done ${COLOR_OFF}"
-        elif [ "$choice" == "Npm intellisense" ]; then
-            code --install-extension christian-kohler.npm-intellisense
-            echo -e "${GREEN} Npm intellisense installation done ${COLOR_OFF}"
-        elif [ "$choice" == "ESLint" ]; then
-            code --install-extension dbaeumer.vscode-eslint
-            echo -e "${GREEN} ESLint installation done ${COLOR_OFF}"
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    if [ "$CHOIX" == "See electron installations" ]; then
+        eval SELECTION=( $(whiptail --title "Electron" --checklist \
+        "Choose what you want to install" 25 80 8 \
+        "Node.js" "" ON \
+        "NPM" "" ON \
+        "Npm intellisense" "" OFF \
+        "ESLint" "" OFF 3>&1 1>&2 2>&3))
+
+        if [ "${#SELECTION[@]}" != 0 ]; then
+            for choice in "${SELECTION[@]}"; do
+                if [ "$choice" == "Node.js" ]; then
+                    sudo apt-get install -y nodejs
+                    echo -e "${GREEN} Node.js installation done ${COLOR_OFF}"
+                elif [ "$choice" == "NPM" ]; then
+                    sudo apt install -y npm
+                    echo -e "${GREEN} NPM installation done ${COLOR_OFF}"
+                elif [ "$choice" == "Npm intellisense" ]; then
+                    code --install-extension christian-kohler.npm-intellisense
+                    echo -e "${GREEN} Npm intellisense installation done ${COLOR_OFF}"
+                elif [ "$choice" == "ESLint" ]; then
+                    code --install-extension dbaeumer.vscode-eslint
+                    echo -e "${GREEN} ESLint installation done ${COLOR_OFF}"
+                fi
+            done
+            echo -e "${GREEN} Installation complete ${COLOR_OFF}"
+        else
+            echo -e " ${RED} User selected Cancel. ${COLOR_OFF}"
         fi
-    done
-    echo -e "${GREEN} Installation complete ${COLOR_OFF}"
+    elif [ "$CHOIX" == "Init electron rep" ]; then
+        sudo npm init -y
+        sudo npm install --save-dev electron
+        echo -e "${GREEN} Electron initialisation done ${COLOR_OFF}"
+    fi
 else
     echo -e " ${RED} User selected Cancel. ${COLOR_OFF}"
 fi
