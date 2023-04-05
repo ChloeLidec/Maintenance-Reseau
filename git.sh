656ff6a80e8e5de782216f1ce49be6812c114d88
ChloeLidec <chloe.lidec@etu.univ-orleans.fr>
2023-04-05 08:18:54 -0400

    #5 - Ajout boucle while dans git.sh

--- git.sh
+++ git.sh
@@ -9,69 +9,76 @@ GREEN='\033[0;32m'        # Green
 YELLOW='\033[0;33m'       # Yellow
 BLUE='\033[0;34m'         # Blue
 
-CHOIX=$(whiptail --title "Git" --menu \
-"Choose what you want to do" 25 80 8 \
-"Commit modified files from actual directory" "" \`
-"Config" "" \
-"Push" "" \
-"Pull" "" \
-"Status" "" \
-"Create a branch" "" \
-"Merge" "" \
-"Stash" "" \
-"Tag" "" 3>&1 1>&2 2>&3)
 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    if [ "$CHOIX" == "Commit modified files from actual directory" ]; then
-        git add .
-        echo -e " ${BLUE} Enter a commit message ${COLOR_OFF}"
-        read MESSAGE
-        git commit -m "$MESSAGE"
-        echo -e " ${BLUE} Commit done ${COLOR_OFF}"
-    elif [ "$CHOIX" == "Push" ]; then
-        echo -e " ${BLUE} Choose where to push ${COLOR_OFF}"
-        git branch
-        read BRANCHPUSH
-        git push origin $BRANCHPUSH
-    elif [ "$CHOIX" == "Pull" ]; then
-        echo -e " ${BLUE} Choose the source ${COLOR_OFF}"
-        git branch
-        read BRANCHEPULL
-        git pull origin $BRANCHEPULL
-    elif [ "$CHOIX" == "Status" ]; then
-        git status
-    elif [ "$CHOIX" == "Create a branch" ]; then
-        echo -e " ${BLUE} Enter branch's name ${COLOR_OFF}"
-        read BRANCHECREA
-        git switch -c $BRANCHECREA
-        echo -e " ${BLUE} You are now on branch $BRANCHECREA ${COLOR_OFF}"
-    elif [ "$CHOIX" == "Merge" ]; then
-        echo -e " ${BLUE} Choose source branch ${COLOR_OFF}"
-        git BRANCHSOURCE
-        read BRANCHESOURCE
-        echo -e " ${BLUE} Chooose destination branch ${COLOR_OFF}"
-        git branch
-        read BRANCHDEST
-        git merge $BRANCHSOURCE $BRANCHDEST
-        echo -e " ${BLUE} Merge done ${COLOR_OFF}"
-    elif [ "$CHOIX" == "Stash" ]; then
-        git stash
-        echo -e " ${BLUE} Stash done ${COLOR_OFF}"
-    elif [ "$CHOIX" == "Tag" ]; then
-        echo -e " ${BLUE} Enter tag's name ${COLOR_OFF}"
-        read TAG
-        git tag $TAG
-        echo -e " ${BLUE} Tag done ${COLOR_OFF}"
-    elif [ "$CHOIX" == "Config" ]; then
-        echo -e " ${BLUE} Enter your name ${COLOR_OFF}"
-        read NAME
-        git config --global user.name "$NAME"
-        echo -e " ${BLUE} Enter your email ${COLOR_OFF}"
-        read EMAIL
-        git config --global user.email "$EMAIL"
-        echo -e " ${BLUE} Config done ${COLOR_OFF}"
+CHOIX=""
+exitstatus=0
+while [ "$CHOIX" != "Quitter" ] && [ $exitstatus = 0 ];
+do
+    CHOIX=$(whiptail --title "Git" --menu \
+    "Choose what you want to do" 25 80 8 \
+    "Commit modified files from actual directory" "" \`
+    "Config" "" \
+    "Push" "" \
+    "Pull" "" \
+    "Status" "" \
+    "Create a branch" "" \
+    "Merge" "" \
+    "Stash" "" \
+    "Tag" "" \
+    "Quitter" 3>&1 1>&2 2>&3)
+
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        if [ "$CHOIX" == "Commit modified files from actual directory" ]; then
+            git add .
+            echo -e " ${BLUE} Enter a commit message ${COLOR_OFF}"
+            read MESSAGE
+            git commit -m "$MESSAGE"
+            echo -e " ${BLUE} Commit done ${COLOR_OFF}"
+        elif [ "$CHOIX" == "Push" ]; then
+            echo -e " ${BLUE} Choose where to push ${COLOR_OFF}"
+            git branch
+            read BRANCHPUSH
+            git push origin $BRANCHPUSH
+        elif [ "$CHOIX" == "Pull" ]; then
+            echo -e " ${BLUE} Choose the source ${COLOR_OFF}"
+            git branch
+            read BRANCHEPULL
+            git pull origin $BRANCHEPULL
+        elif [ "$CHOIX" == "Status" ]; then
+            git status
+        elif [ "$CHOIX" == "Create a branch" ]; then
+            echo -e " ${BLUE} Enter branch's name ${COLOR_OFF}"
+            read BRANCHECREA
+            git switch -c $BRANCHECREA
+            echo -e " ${BLUE} You are now on branch $BRANCHECREA ${COLOR_OFF}"
+        elif [ "$CHOIX" == "Merge" ]; then
+            echo -e " ${BLUE} Choose source branch ${COLOR_OFF}"
+            git BRANCHSOURCE
+            read BRANCHESOURCE
+            echo -e " ${BLUE} Chooose destination branch ${COLOR_OFF}"
+            git branch
+            read BRANCHDEST
+            git merge $BRANCHSOURCE $BRANCHDEST
+            echo -e " ${BLUE} Merge done ${COLOR_OFF}"
+        elif [ "$CHOIX" == "Stash" ]; then
+            git stash
+            echo -e " ${BLUE} Stash done ${COLOR_OFF}"
+        elif [ "$CHOIX" == "Tag" ]; then
+            echo -e " ${BLUE} Enter tag's name ${COLOR_OFF}"
+            read TAG
+            git tag $TAG
+            echo -e " ${BLUE} Tag done ${COLOR_OFF}"
+        elif [ "$CHOIX" == "Config" ]; then
+            echo -e " ${BLUE} Enter your name ${COLOR_OFF}"
+            read NAME
+            git config --global user.name "$NAME"
+            echo -e " ${BLUE} Enter your email ${COLOR_OFF}"
+            read EMAIL
+            git config --global user.email "$EMAIL"
+            echo -e " ${BLUE} Config done ${COLOR_OFF}"
+        fi
+    else
+        echo "${RED} You chose Cancel. ${COLOR_OFF}"
     fi
-else
-    echo "${RED} You chose Cancel. ${COLOR_OFF}"
-fi
+done
